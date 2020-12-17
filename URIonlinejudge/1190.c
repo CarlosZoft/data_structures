1daba16db6697fe91cec0f1f60bc947db13861f4
Carlos Rafael <carlosoftwar@gmail.com>
2020-12-17 16:37:05 +0000

    1190 solution C

---
+++ URIonlinejudge/1190.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    float matriz[12][12];
+    char escolha;
+    int aux = 10, aux1 = 1, aux2 = 11, sum = 0;
+    scanf("%c", escolha);
+
+    //preencher matriz
+    for (int lin = 0; lin < 12; lin++)
+    {
+        for (int col = 0; col < 12; col++)
+        {
+            scanf("%lf", &matriz[lin][col]);
+        }
+    }
+
+    //Soma matriz
+    while (aux2 > 6)
+    {
+        int col = aux2;
+        for (int lin = aux1; lin <= aux; lin++)
+        {
+            sum += matriz[lin][col];
+        }
+        aux2--;
+        aux--;
+        aux1++;
+    }
+    if (escolha == 'S')
+        printf("%.1lf\n", sum);
+    else if (escolha == 'M')
+        printf("%.1lf\n", sum / 30);
+
+    return 0;
+}
