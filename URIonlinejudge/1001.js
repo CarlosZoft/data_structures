9fae98dfda5259385f3f043d0e2885c287fc7691
Carlos <carlosoftwar@gmail.com>
2021-01-06 22:11:19 -0300

    solution in JS

---
+++ URIonlinejudge/1001.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let A = parseInt(lines.shift())
+let B = parseInt(lines.shift())
+console.log('X =', A+B)
